9aef90b8b5d5402b0bc034a8b3490ec2fcd48b4a
AjayTumula <ajaytumula72@gmail.com>
2024-01-27 07:45:39 +0000

    used map function for contacts array

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+
+function createCard(contacts) {
+  return (
+    <Card 
+      name={contacts.name}
+      img={contacts.imgURL}
+    />
+  )
+}
+
+
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card 
+      <Avatar 
+        img="https://media.licdn.com/dms/image/C5603AQEOIPpjQcUb6Q/profile-displayphoto-shrink_800_800/0/1542597037878?e=2147483647&v=beta&t=EqqW7qYkANpnsPqV03K3fuPn4RSUnHe5xZmoYgdLx2A"
+      />
+
+      {contacts.map(createCard)}
+
+      {/* <Card 
         name= {contacts[0].name}
         img= {contacts[0].imgURL}
         tel= {contacts[0].phone}
@@ -23,7 +42,7 @@ function App() {
         img= {contacts[2].imgURL}
         tel= {contacts[2].phone}
         email= {contacts[2].email}
-      />
+      /> */}
     </div>
   );
 }
